dcb2ff6930f552929a980c1a7c23b3c3b27c4a95
Trent Dodson <dodson-7-7@outlook.com>
2022-04-10 19:41:16 -0400

    added char array and updated file path for js in html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
 // Assignment code here
 
+// Character Array
+const upperCaseChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const lowerCaseChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const numberChar = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+const symbolChar = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "=", "+", "{", "}", "[", "]", ":", "|", ";", "<", ",", ">", "?", "/", "."];
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
